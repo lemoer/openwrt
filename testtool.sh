33c87e55d2f2649b23348c8430eacf46198791db
lemoer <git@irrelefant.net>
2025-10-03 03:42:08 +0200

    testtool.sh: add all dependencies

--- testtool.sh
+++ testtool.sh
@@ -29,12 +29,184 @@ copy_staging_dir_fromtool() {
 # TODO: find out if CONFIG_... variables can leak in
 
 copy_staging_dir_fromtool 000-meta-prereq
-if [ $toolname = "patch" ] || [ $toolname = "tar" ] || [ $toolname = "zstd" ] || [ $toolname = "m4" ]; then
+
+if [ $toolname != "libdeflate" ]; then
     copy_staging_dir_fromtool libdeflate
 fi
-if [ $toolname = "autoconf" ]; then
-    copy_staging_dir_fromtool m4
-fi
+
+# The following are the dependencies between tools, as far as I could
+# figure out.
+# For each dependency, call copy_staging_dir_fromtool with the dependency name.
+
+case "$toolname" in
+    autoconf)
+        copy_staging_dir_fromtool m4
+        ;;
+    automake)
+        copy_staging_dir_fromtool autoconf
+        copy_staging_dir_fromtool pkgconf
+        copy_staging_dir_fromtool xz
+        ;;
+    b43-tools)
+        copy_staging_dir_fromtool bison
+        ;;
+    bc)
+        copy_staging_dir_fromtool bison
+        copy_staging_dir_fromtool libtool
+        ;;
+    bison)
+        copy_staging_dir_fromtool flex
+        ;;
+    bzip2)
+        copy_staging_dir_fromtool cmake
+        ;;
+    cbootimage)
+        copy_staging_dir_fromtool automake
+        ;;
+    cmake)
+        copy_staging_dir_fromtool libressl
+        copy_staging_dir_fromtool ninja
+        copy_staging_dir_fromtool expat
+        copy_staging_dir_fromtool xz
+        copy_staging_dir_fromtool zlib
+        copy_staging_dir_fromtool zstd
+        ;;
+    coreutils)
+        copy_staging_dir_fromtool automake
+        copy_staging_dir_fromtool bison
+        copy_staging_dir_fromtool gnulib
+        ;;
+    dosfstools)
+        copy_staging_dir_fromtool automake
+        ;;
+    e2fsprogs)
+        copy_staging_dir_fromtool libtool
+        copy_staging_dir_fromtool util-linux
+        ;;
+    elfutils)
+        copy_staging_dir_fromtool bison
+        copy_staging_dir_fromtool gnulib
+        copy_staging_dir_fromtool m4
+        copy_staging_dir_fromtool zlib
+        ;;
+    erofs-utils)
+        copy_staging_dir_fromtool libtool
+        copy_staging_dir_fromtool xz
+        copy_staging_dir_fromtool lz4
+        copy_staging_dir_fromtool util-linux
+        ;;
+    fakeroot)
+        copy_staging_dir_fromtool libtool
+        ;;
+    findutils)
+        copy_staging_dir_fromtool bison
+        ;;
+    firmware-utils)
+        copy_staging_dir_fromtool cmake
+        ;;
+    flex)
+        copy_staging_dir_fromtool libtool
+        ;;
+    genext2fs)
+        copy_staging_dir_fromtool libtool
+        ;;
+    gengetopt)
+        copy_staging_dir_fromtool libtool
+        ;;
+    gmp)
+        copy_staging_dir_fromtool libtool
+        ;;
+    isl)
+        copy_staging_dir_fromtool gmp
+        ;;
+    liblzo)
+        copy_staging_dir_fromtool cmake
+        ;;
+    libressl)
+        copy_staging_dir_fromtool pkgconf
+        ;;
+    libtool)
+        copy_staging_dir_fromtool automake
+        copy_staging_dir_fromtool gnulib
+        copy_staging_dir_fromtool missing-macros
+        ;;
+    lz4)
+        copy_staging_dir_fromtool meson
+        ;;
+    lzma-old)
+        copy_staging_dir_fromtool zlib
+        ;;
+    lzop)
+        copy_staging_dir_fromtool cmake
+        copy_staging_dir_fromtool liblzo
+        ;;
+    llvm-bpf)
+        copy_staging_dir_fromtool cmake
+        ;;
+    make-ext4fs)
+        copy_staging_dir_fromtool zlib
+        ;;
+    meson)
+        copy_staging_dir_fromtool ninja
+        ;;
+    missing-macros)
+        copy_staging_dir_fromtool autoconf
+        ;;
+    mkimage)
+        copy_staging_dir_fromtool bison
+        copy_staging_dir_fromtool libressl
+        ;;
+    mklibs)
+        copy_staging_dir_fromtool libtool
+        ;;
+    mold)
+        copy_staging_dir_fromtool cmake
+        copy_staging_dir_fromtool zlib
+        copy_staging_dir_fromtool zstd
+        ;;
+    mpc)
+        copy_staging_dir_fromtool mpfr
+        copy_staging_dir_fromtool gmp
+        ;;
+    mpfr)
+        copy_staging_dir_fromtool gmp
+        ;;
+    mtd-utils)
+        copy_staging_dir_fromtool libtool
+        copy_staging_dir_fromtool zlib
+        copy_staging_dir_fromtool util-linux
+        ;;
+    padjffs2)
+        copy_staging_dir_fromtool findutils
+        ;;
+    patchelf)
+        copy_staging_dir_fromtool libtool
+        ;;
+    pkgconf)
+        copy_staging_dir_fromtool meson
+        ;;
+    quilt)
+        copy_staging_dir_fromtool autoconf
+        copy_staging_dir_fromtool findutils
+        ;;
+    sdcc)
+        copy_staging_dir_fromtool bison
+        ;;
+    squashfs3-lzma)
+        copy_staging_dir_fromtool lzma-old
+        ;;
+    squashfs4)
+        copy_staging_dir_fromtool xz
+        copy_staging_dir_fromtool zlib
+        ;;
+    util-linux)
+        copy_staging_dir_fromtool bison
+        copy_staging_dir_fromtool automake
+        ;;
+    yafut)
+        copy_staging_dir_fromtool cmake
+        ;;
+esac
 
 copyit include
 copyit rules.mk
