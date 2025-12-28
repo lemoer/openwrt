eb74f9eae31f93330d836485c168a65481465525
lemoer <git@irrelefant.net>
2025-12-28 12:35:13 +0100

    tmpdiffoscope: specify paths using regex

--- tmpdiffoscope.sh
+++ tmpdiffoscope.sh
@@ -5,6 +5,6 @@ set -e
 # can be e.g. host/bin/pkgconf
 toolpath=$1
 
-tmpname=$(find -L tmpcurrent | grep $toolpath)
+tmpname=$(find -L tmpcurrent | grep -e "$toolpath\$")
 
 diffoscope --text-color=always staging_dir/$toolpath $tmpname | less --raw-control-chars
